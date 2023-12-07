8b46db1768818064703da421f30bde6edd61cdaa
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-07 11:41:50 +0530

    Add files via upload

---
+++ set_py.py
@@ -0,0 +1,23 @@
+# set
+d={1,2,3,8,6,4,2}
+print(d)
+
+# add function
+a = {1,2,3,6}
+a.add(5)
+print(a)
+
+# add in list for append function
+j=[1,2,5,3,6]
+j.append(56)
+print(j)
+
+# remove_fn
+t = {1,2,3,4}
+t.remove(3)
+print(t)
+
+# pop_fn
+i = [1,2,3]
+i.pop(2)
+print(i)
